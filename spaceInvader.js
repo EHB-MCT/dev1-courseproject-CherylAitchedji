a6ba37b64378bfcde6cffccd6d52081309b5c700
Cheryl A <cheryl.aitchedji@student.ehb.be>
2024-10-01 19:40:07 +0200

    SpaceInvader: Signature Opdracht

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,26 @@
-"use strict";
+//oproep functie
+signature();
 
-alert("Draw your space invader here");
+//functie definitie
+function signature() {
+  let canvas = document.querySelector("canvas");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  let context = canvas.getContext("2d");
+
+  //achtergrond
+  context.fillStyle = "black";
+
+  context.fillRect(50, 50, 300, 300);
+  context.fill();
+
+  //spaceinvader (links-rechts)
+  context.fillStyle = "#d468c1";
+
+  context.fillRect(75, 175, 50, 150);
+  context.fillRect(125, 75, 50, 200);
+  context.fillRect(175, 75, 50, 50);
+  context.fillRect(175, 225, 50, 100);
+  context.fillRect(225, 75, 50, 200);
+  context.fillRect(275, 175, 50, 150);
+}
