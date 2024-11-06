46468959206e5df608c015f431df93cfe9c9eba5
Cheryl A <cheryl.aitchedji@student.ehb.be>
2024-11-06 12:30:56 +0100

    bubble has a randomized position

--- projects/GenerativeDesign/gen_design.js
+++ projects/GenerativeDesign/gen_design.js
@@ -6,15 +6,19 @@ import * as Utils from "../../scripts/utils.js";
 //functieoproep
 generateBubbles();
 
-//functie definitie
+// functie definitie
 function generateBubbles() {
-  //maakt zwart achtergrond met als arg breedte en hoogte van scherm
+  // variabelen deklareren (hoogte + breedte van container (scherm), random pos voor bubble)
   let width = canvas.width;
   let height = canvas.height;
+  let rxpos = width * Math.random();
+  let rypos = height * Math.random();
+
+  // maakt zwart achtergrond met als arg breedte en hoogte van scherm
   context.fillRect(0, 0, width, height);
 
-  //tekent circle
+  // tekent circle
   context.strokeStyle = "white";
   context.lineWidth = 7;
-  Utils.strokeEllipse(width / 2, height / 2, 100, 100);
+  Utils.strokeEllipse(rxpos, rypos / 2, 200, 200);
 }
