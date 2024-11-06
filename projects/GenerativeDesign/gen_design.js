55862e72d137d70dda2479e8b796bc279776cd8b
Cheryl A <cheryl.aitchedji@student.ehb.be>
2024-11-06 12:00:28 +0100

    eerste bubble

--- projects/GenerativeDesign/gen_design.js
+++ projects/GenerativeDesign/gen_design.js
@@ -8,7 +8,12 @@ generateBubbles();
 
 //functie definitie
 function generateBubbles() {
+  //maakt zwart achtergrond met als arg breedte en hoogte van scherm
   let width = canvas.width;
   let height = canvas.height;
   context.fillRect(0, 0, width, height);
+
+  //tekent bubble
+  context.fillStyle = "white";
+  Utils.fillAndStrokeCircle(width / 2, height / 2, 20);
 }
