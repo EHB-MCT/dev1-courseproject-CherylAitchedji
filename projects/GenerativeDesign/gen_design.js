cbfd3dbf54e66696f12ed25b30188bde8270184a
Cheryl A <cheryl.aitchedji@student.ehb.be>
2024-11-06 11:54:11 +0100

    testing if canvas is working

--- projects/GenerativeDesign/gen_design.js
+++ projects/GenerativeDesign/gen_design.js
@@ -2,3 +2,11 @@
 
 import context from "../../scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
+
+//functieoproep
+generateBubbles();
+
+//functie definitie
+function generateBubbles() {
+  context.fillRect(0, 0, 200, 200);
+}
