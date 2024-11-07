58dd37af67b8fe5ba40ba42516bea451087bec63
Cheryl A <cheryl.aitchedji@student.ehb.be>
2024-11-07 19:55:17 +0100

    Design bubble

--- projects/GenerativeDesign/gen_design.js
+++ projects/GenerativeDesign/gen_design.js
@@ -3,6 +3,9 @@
 import context from "../../scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
 
+//Linewidth
+let n = 10;
+
 // variabelen deklareren
 //hoogte + breedte van container (scherm)
 let width = canvas.width;
@@ -12,27 +15,32 @@ let random_r = Math.random() * 100;
 //functieoproep
 draw();
 
+////////////////////////////Canvas//////////////////////////////
 function draw() {
   // stijl voor cirkel
   // maakt zwart achtergrond met als arg breedte en hoogte van scherm
   context.fillRect(0, 0, width, height);
   context.strokeStyle = "white";
-  context.lineWidth = 7;
-  generateBubble();
+  context.lineWidth = n;
+
+  //  generateBubbles(100, 200, 50);
+  bubble(width / 2, height / 2, 200);
 }
 
-// functie definitie van generateBubble
-function generateBubble() {
-  // aantal cirkels
-  let n = 50;
-  //Teken cirkels
-  for (let i = 0; i < n; i++) {
-    //geeft elk cirkel een kleur tussen geel tot paars
-    context.fillStyle = Utils.hsla(Utils.randomNumber(60, 300), 100, 70, 70);
-    Utils.fillAndStrokeCircle(
-      width * Math.random(),
-      height * Math.random(),
-      Math.random() * 100
-    );
-  }
+function generateBubbles() {}
+
+////////////////////////////Bubble//////////////////////////////
+function bubble(x, y, size) {
+  //omtrek bubble
+  Utils.strokeCircle(x, y, size);
+  //neemt kleur tussen geel tot paars
+  context.strokeStyle = Utils.hsla(Utils.randomNumber(130, 330), 100, 70, 70);
+  //highlight 1
+  Utils.strokeCircle(x, y, size - n);
+  //highlight 2
+  context.strokeStyle = Utils.hsla(Utils.randomNumber(50, 120), 100, 70, 70);
+  Utils.strokeCircle(x, y, size - n * 2);
+  //Spark
+  context.fillStyle = "white";
+  Utils.fillCircle(x + size / 2, y - size / 3, size / 5);
 }
